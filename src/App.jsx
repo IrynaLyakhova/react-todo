87dfc95e279e764c0c20cefa6e11359933d323fe
IrynaLyakhova <irynalyakhova@gmail.com>
2024-02-25 15:03:38 -0800

    Create the beginnings of the todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const todoList = [
+  {
+    id: 1,
+    title: "Commit project and create working branch"
+  },
+  {
+    id: 2,
+    title: "Create the beginnings of the todo list"
+  },
+  {
+    id: 3,
+    title: "Complete assignment"
+  }
+];
 
+function listItem (item) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <li key={item.id}>
+      <span>{item.id} </span>
+      <span>{item.title}</span>
+    </li>
+ )
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <hr /> 
+
+      <ul>
+      {todoList.map(listItem)}   
+      </ul>
+    </div> 
+  );
+}
+
+export default App;
